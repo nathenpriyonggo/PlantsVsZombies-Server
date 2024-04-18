058ebcb5e3ea65b7928898e587a760245b2c4beb
mohammed <mlnu31@uic.edu>
2024-04-18 17:59:01 -0500

    I already made the grid class

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -1,7 +1,40 @@
 import java.util.ArrayList;
 
 public class Grid {
+    private ArrayList<ArrayList<Element>> grid;
 
-    ArrayList<ArrayList<String>> grid = new ArrayList<ArrayList<String>>();
+    public Grid() {
+        this.grid = new ArrayList<>();
+        initializeGrid();
+    }
+
+    private void initializeGrid() {
+        for (int i = 0; i < 7; i++) {
+            ArrayList<Element> row = new ArrayList<>();
+            for (int j = 0; j < 7; j++) {
+                row.add(new Element("player", "opp", i, j, 0, 0, ""));
+            }
+            grid.add(row);
+        }
+    }
+
+    public void setElement(int x, int y, Element element) {
+        grid.get(x).set(y, element);
+    }
+
+    public Element getElement(int x, int y) {
+        return grid.get(x).get(y);
+    }
+
+    public void printGrid() {
+        for (ArrayList<Element> row : z) {
+            for (Element element : row) {
+                System.out.print(element.getElementState() + " ");
+            }
+            System.out.println();
+        }
+    }
 
 }
+
+
